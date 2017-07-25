9bc8a0bacb7879d236e6daec3f83545d7c6c82f1
padma <padma@srikanths-mbp.attlocal.net>
2017-07-24 20:02:07 -0700

    first today

--- PCA_R.R
+++ PCA_R.R
@@ -1,3 +1,6 @@
+#Questions: 
+#does the calling of the column using $ work?
+#does the subtraction from a column work?
 
 #getting averages from each column 
 getAverages <- function(raw_data_table) {
@@ -15,11 +18,12 @@ subtractAveragesFromVariables <- function(raw_data_table, mean_values) {
   col_names <- colnames(raw_data_table)
   i <- 1
   for (mean in mean_values) { 
+    #gets the current mean
     current_mean <- mean_values[[i]]
+    #changes corresponding column all at once 
+    raw_data_table$col_names[[i]] <- raw_data_table$col_names[[i]] - current_mean
     i = i + 1
-    for (col in col_names) {
-      raw_data_table$col_names[[i]] <- raw_data_table$col_names[[i]] - current_mean
-    }
   }
   return(raw_data_table)
 }
+
