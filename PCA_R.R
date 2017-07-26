9270e7eab305d18b2639c10f62ddf3b9ac725f52
padma <padma@srikanths-mbp.attlocal.net>
2017-07-26 01:02:04 -0700

    fully debugged

--- PCA_R.R
+++ PCA_R.R
@@ -1,6 +1,6 @@
 #Questions: 
-#does the calling of the column using $ work?
-#does the subtraction from a column work?
+#does the calling of the column using $ work? y 
+#does the subtraction from a column work? y 
 #check covariance matrix item setting 
 #is sort destructive? or do you have to set into another variable?
 #is it transposed to the correct matrix?
@@ -12,7 +12,7 @@ getAverages <- function(raw_data_table) {
   mean_values <- list()
   i <- 1
   for (col in col_names) { 
-    mean_values[[i]] <- mean(raw_data_table$colnames[i])
+    mean_values[[i]] <- mean(raw_data_table[[col_names[i]]])
     i <- i + 1
   }
   return(mean_values)
@@ -25,7 +25,7 @@ subtractAveragesFromVariables <- function(raw_data_table, mean_values) {
     #gets the current mean
     current_mean <- mean_values[[i]]
     #changes corresponding column all at once 
-    raw_data_table$col_names[[i]] <- raw_data_table$col_names[[i]] - current_mean
+    raw_data_table[[col_names[i]]] <- raw_data_table[[col_names[i]]] - current_mean
     i = i + 1
   }
   return(raw_data_table)
@@ -39,12 +39,13 @@ outputCovarianceTable <- function(subtracted_mean_table) {
   #use col_names to make sure the iteration happens the correct number of times 
   for (col in col_names) { 
     for (col in col_names) { 
-      curr_cov = cov(subtracted_mean_table$col_names[[i]], subtracted_mean_table$col_names[[j]])
+      curr_cov = cov(subtracted_mean_table[,i], subtracted_mean_table[,j])
       #set matrix value 
       covariance_matrix[i, j] <- curr_cov
       j <- j + 1
     }
     i <- i + 1
+    j <- 1
   } 
   return(covariance_matrix)
 }
@@ -55,14 +56,14 @@ getEigenVecs <- function(covariance_matrix) {
   return(eigen_vectors)
 }
 
-outputFinalData <- function(eigen_vecs, raw_data_table) {
-  return(t(eigen_vecs) %*% data.matrix(raw_data_table))
+outputFinalData <- function(eigen_vecs, subtracted_data_table) {
+  return(t(t(eigen_vecs) %*% t(data.matrix(subtracted_data_table))))
 }
 
 runPCA <- function(raw_data_table) { 
   subtracted_mean_data <- subtractAveragesFromVariables(raw_data_table, getAverages(raw_data_table))
   cov_matrix <- outputCovarianceTable(subtracted_mean_data)
-  final_data <- outputFinalData(getEigenVecs(cov_matrix), raw_data_table)
+  final_data <- outputFinalData(getEigenVecs(cov_matrix), subtracted_mean_data)
   return(final_data)
 }
 
